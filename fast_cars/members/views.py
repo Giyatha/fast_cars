d22c5ac3624cab3767827865b055afccd1b783e0
unknown <yusufgiyatha@gmail.com>
2024-03-11 14:13:27 +0100

    css dun dey work

--- fast_cars/members/views.py
+++ fast_cars/members/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-  template = loader.get_template('home.html')
+  template = loader.get_template('base.html')
   return HttpResponse(template.render())
